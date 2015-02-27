4a0aa7bf9515d56a7b39132a4044c1abaeead7e4
ralphtandetzky2 <ralph.tandetzky2@googlemail.com>
2015-02-27 12:09:53 +0100

    Bugfix: Now works for odd length of input signal.

--- dimf_initial_guess.m
+++ dimf_initial_guess.m
@@ -2,7 +2,7 @@ function [ampl,phase] = dimf_initial_guess( signal )
   N = length(signal);
   dft = fft(signal);
   spec = abs(dft);
-  [ampl,freq] = max(spec(2:end/2));
+  [ampl,freq] = max(spec(2:floor(end/2)));
   ampl = 2 * ampl / N;
   phase = (0:(length(signal)-1)) / N * 2 * pi * freq + angle(dft(freq+1));  
 end
